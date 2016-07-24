970a71f062cca56866ec95552a7cfba28103205a
zldevil2011 <929034478@qq.com>
2016-07-24 22:18:26 +0800

    init project

---
+++ zhengsheng/app/src/test/java/com/newly_dawn/app/zhengsheng/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.newly_dawn.app.zhengsheng;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
