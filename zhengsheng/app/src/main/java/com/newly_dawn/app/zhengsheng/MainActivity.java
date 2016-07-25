1e4732f093fc4898f283a5420f67516c0d778a06
zldevil2011 <929034478@qq.com>
2016-07-25 11:24:09 +0800

    init welcome page

--- zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/MainActivity.java
+++ zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/MainActivity.java
@@ -1,13 +1,25 @@
 package com.newly_dawn.app.zhengsheng;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+import com.romainpiel.shimmer.Shimmer;
+import com.romainpiel.shimmer.ShimmerTextView;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ShimmerTextView versionNumber= (ShimmerTextView)findViewById(R.id.versionNumber);
+        ShimmerTextView zhengshenge= (ShimmerTextView) findViewById(R.id.welcome);
+
+        Shimmer shimmer = new Shimmer();shimmer.start(versionNumber);
+        Shimmer zhengshenge_shimmer = new Shimmer();zhengshenge_shimmer.start(zhengshenge);
     }
 }
