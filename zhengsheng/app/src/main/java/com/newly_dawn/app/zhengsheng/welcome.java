9d0470860a3202812913395783b16f80b7d7d6fa
zldevil2011 <929034478@qq.com>
2016-07-25 13:07:06 +0800

    go to main page after welcome

--- zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/welcome.java
+++ zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/welcome.java
@@ -1,22 +1,32 @@
 package com.newly_dawn.app.zhengsheng;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerTextView;
 
-public class welcome extends Activity {
 
+public class welcome extends Activity {
+    Handler handler = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         ShimmerTextView versionNumber= (ShimmerTextView)findViewById(R.id.versionNumber);
         ShimmerTextView zhengshenge= (ShimmerTextView) findViewById(R.id.welcome);
-
         Shimmer shimmer = new Shimmer();shimmer.start(versionNumber);
         Shimmer zhengshenge_shimmer = new Shimmer();zhengshenge_shimmer.start(zhengshenge);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(welcome.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
