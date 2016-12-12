bb1a73d3e97dbd0327bfcc2d2ade88c34cf7f2cc
zhaolong <zhaolong@zhaolongdeMacBook-Pro.local>
2016-12-12 11:44:04 +0800

    get workorderlsit

--- zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/user/WorkOrder.java
+++ zhengsheng/app/src/main/java/com/newly_dawn/app/zhengsheng/user/WorkOrder.java
@@ -12,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +29,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class WorkOrder extends AppCompatActivity {
@@ -86,8 +90,29 @@ public class WorkOrder extends AppCompatActivity {
             } else {
                 if (result.get("code").equals("200")) {
                     try {
-                        TextView workOrderText = (TextView)findViewById(R.id.workOrderText);
-                        workOrderText.setText(result.get("text"));
+                        ListView workOrderList = (ListView)findViewById(R.id.workOrderList);
+                        JSONObject jsonObject = new JSONObject(result.get("text"));
+                        Log.i("zhaolong", String.valueOf(jsonObject));
+                        JSONArray alertList = jsonObject.getJSONArray("workOrderList");
+                        Log.i("zhaolong", String.valueOf(alertList));
+                        List<Map<String,String>> listItems = new ArrayList<>();
+                        int len = alertList.length();
+                        for(int i = 0; i < len; ++i){
+                            Map<String, String> map = new HashMap<>();
+                            String num = alertList.getJSONObject(i).getString("num");
+                            String time = alertList.getJSONObject(i).getString("time");
+                            String content = alertList.getJSONObject(i).getString("content");
+                            String status = alertList.getJSONObject(i).getString("status");
+                            map.put("work_order_content", content);
+                            map.put("work_order_status", "状态:" + status + ",  ");
+                            map.put("work_order_number", "编号:" +num + ",  ");
+                            map.put("work_order_time", time);
+                            listItems.add(map);
+                        }
+                        SimpleAdapter adapter = new SimpleAdapter(WorkOrder.this, listItems, R.layout.workorder_list_item,
+                                new String[]{"work_order_content", "work_order_status", "work_order_number","work_order_time"},
+                                new int[]{R.id.work_order_content, R.id.work_order_status, R.id.work_order_number, R.id.work_order_time});
+                        workOrderList.setAdapter(adapter);
                     } catch (Exception e) {
                         Log.i("zhengsheng_exp2", String.valueOf(e));
                         e.printStackTrace();
